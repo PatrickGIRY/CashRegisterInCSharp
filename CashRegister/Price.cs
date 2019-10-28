2950c5b1bb858c6d46ba5fa73d187622f3fa84db
Patrick GIRY <patrickgiry@gmail.com>
2019-10-28 15:18:20 +0100

    Third implementation with Price value type static factory method

--- CashRegister/Price.cs
+++ CashRegister/Price.cs
@@ -1,32 +1,35 @@
 
-namespace CashRegister {
-    public sealed class Price
+namespace CashRegister
+{
+  public class Price
+  {
+    private readonly double value;
+
+    public static Price ValueOf(double value) { return new Price(value); }
+
+    private Price(double value) { this.value = value; }
+
+    public static Price operator *(Price price, double quantity)
     {
-        private readonly double value;
-
-        public Price(double value) { this.value = value; }
-
-        public static Price operator *(Price price, double quantity)
-        {
-          return new Price(price.value * quantity);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (this == obj) return true;
-            if (obj == null || this.GetType() != obj.GetType()) return false;
-            Price price = obj as Price;
-            return price.value.Equals(value);
-        }
-
-        public override string ToString()
-        {
-            return "Price{ value=" + value + '}';
-        }
-
-        public override int GetHashCode()
-        {
-            return value.GetHashCode();
-        }
+      return new Price(price.value * quantity);
     }
+
+    public override bool Equals(object obj)
+    {
+      if (this == obj) return true;
+      if (obj == null || this.GetType() != obj.GetType()) return false;
+      Price price = obj as Price;
+      return price.value.Equals(value);
+    }
+
+    public override string ToString()
+    {
+      return "Price{ value=" + value + '}';
+    }
+
+    public override int GetHashCode()
+    {
+      return value.GetHashCode();
+    }
+  }
 }
